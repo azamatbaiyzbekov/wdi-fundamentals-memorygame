54387bcd458e244f8200442b4cd4d251846b3b31
Azamat Baiyzbekov <azamatbaiyzbekov@Azamats-MacBook-Pro.local>
2019-05-14 17:36:35 -0700

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,21 +1,30 @@
 var cards = ["queen", "queen", "king", "king"];
-
 var cardsInPlay = [];
+
+cardsInPlay.push(cards[cardId]);
 cardsInPlay.push(cardOne);
 cardsInPlay.push(cardTwo);
 
-var cardOne = cards[0];
-console.log("User flipped queen");
+function checkForMatch () {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
+}
+};
 
-var cardTwo = cards[2];
-console.log("User flipped king");
-
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[2])
-		alert("You found a match!");
-	else {
-		alert("Sorry, try again.");
+function flipCard (cardId) {
+	if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1])
+		alert("You found a match!"); }
+	else { alert("Sorry, try again.");
+	console.log ("User flipped" + cards[cardId]);
 	}
 	
 
 }
+
+};
+
+flipCard(0);
+flipCard(2);
